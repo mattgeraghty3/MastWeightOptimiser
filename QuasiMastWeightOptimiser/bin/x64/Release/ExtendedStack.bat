96653ac909591c21c6a7316a18101285b918ef0e
mattgeraghty3 <32271279+mattgeraghty3@users.noreply.github.com>
2018-05-04 15:29:16 +0100

    Add files via upload

---
+++ QuasiMastWeightOptimiser/bin/x64/Release/ExtendedStack.bat
@@ -0,0 +1,2 @@
+cd /d %~dp0
+cmd /k EDITBIN /stack:10000000 MastWeightOptimizer.exe MastLocations.csv TurbineLocationsAndMastWeights.csv BinInformation.csv
---
+++ QuasiMastWeightOptimiser/bin/x64/Release/RunQuasiMastWeightOptimizer.bat
@@ -0,0 +1,2 @@
+cd /d %~dp0
+cmd /k QuasiMastWeightOptimizer.exe MastLocations.csv TurbineLocationsAndMastWeights.csv BinInformation.csv
